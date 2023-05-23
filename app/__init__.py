2c3609113ea792e112524c1e9f5a042ca161f2fc
Tony <iamt.he@yahoo.com>
2023-05-23 16:41:44 -0700

    pushed. has errors in routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,4 @@ import os
 app = Flask(__name__)
 app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
 app.register_blueprint(routes.bp)
+
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
 from flask import Blueprint, render_template
-
+import os
+import sqlite3
+from datetime import datetime
 
 bp = Blueprint("main", __name__, "/")
+DB_FILE = os.environ.get("DB_FILE")
 
 @bp.route('/')
 def main():
-    return render_template("main.html")
+    with sqlite3.connect(DB_FILE) as conn:
+        curs=conn.cursor()
+        curs.execute("SELECT id, name ,start_datetime,end_datetime FROM appointments ORDER BY start_datetime")
+        rows=curs.fetchall()
+
+    for row in rows:
+        # start_time= datetime.strptime(row[2],'%Y-%m-%d %H:%M:%S')
+        start_time=str(row[2])[7:]
+        row[2]=start_time.strftime(start_time,'%H:%M:%S')
+        # end_time=datetime.strptime(row[3],'%Y-%m-%d %H:%M:%S' )
+        # row[3]=end_time[7:].strftime(row[3],'%H:%M:%S')
+   
+
+    return render_template("main.html", rows=rows)
